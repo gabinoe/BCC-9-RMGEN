48eab0220dad141a509e14f31ee9c497fb139512
gabinoe <118677791+gabinoe@users.noreply.github.com>
2023-07-13 19:32:13 -0600

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -5,13 +5,46 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub ?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email ?',
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project',
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function for README
+async function writeToFile(fileName, data) {
+  fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize
+async function init() {
+  try {
+    const inquirerResponses = await inquirer.prompt(questions);
+    console.log('Generating README...');
+    const mardownContent = generateMarkdown(inquirerResponses);
+    await writeToFile('README.md', markdownContent);
+    console.log('README generated successfully.');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
 
-// Function call to initialize app
+// call to start the application
 init();
