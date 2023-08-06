b6f1af33e61b226530f4fab5b62f91d376247538
gabinoe <gabinoptg@gmail.com>
2023-08-06 15:35:36 -0600

    update js

--- Develop/index.js
+++ Develop/index.js
@@ -26,6 +26,34 @@ const questions = [
     name: 'description',
     message: 'Please write a short description of your project',
   },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?',
+  },
 ];
 
 // Function for README
@@ -46,5 +74,4 @@ async function init() {
   }
 }
 
-// call to start the application
 init();
